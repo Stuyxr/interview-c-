2a073bebd1c04b918a37eb92e4c00125110766dd
stuyxr <314920465@qq.com>
2023-02-22 14:04:39 +0800

    202302

--- 2_5.cpp
+++ 2_5.cpp
@@ -4,6 +4,9 @@ class parent {
     int* p;
     parent() { printf("constructing parent\n"); }
     parent(const parent& other) { printf("copying parent\n"); }
+    virtual void print() {
+        printf("parent\n");
+    }
     ~parent() { printf("deleting parent\n"); }
 };
 class child : public parent {
@@ -11,12 +14,16 @@ class child : public parent {
     child() { printf("constructing child\n"); }
     child(const child& other) { printf("copying child\n"); }
     ~child() { printf("deleting child\n"); }
+    virtual void print() {
+        printf("child\n");
+    }
 };
 int main() {
     // child* p = new child();
     // delete p;
-    child a;
-    child b = a;
+    parent* p = new parent();
+    p->print();
+    delete p;
     /*
     constructing parent
     constructing child
